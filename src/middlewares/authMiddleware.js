890a853aebe02d6463c25b716922545bfd5b99fc
Shital769 <110831382+Shital769@users.noreply.github.com>
2023-03-07 16:22:00 +1100

    started authentication using jwt

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+export const isAuth = (req, res, next) => {
+  try {
+    //authorization code process
+    //get jwt from header 
+    // check jwt validation in DB
+    // check if the payload in jwt matches in our admi user
+    
+
+    const authorized = false;
+
+    authorized
+      ? next()
+      : res.status(403).json({
+          status: "error",
+          message: "",
+        });
+  } catch (error) {
+    next(error);
+  }
+};
